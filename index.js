bd4e96213abeef17f5995f830e84126718d66d61
fthomp8044 <fthomp2430@gmail.com>
2020-01-01 21:37:53 -0500

    added drum sounds

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 //set a variable to let the querySelectorAll of the drums by the length of how many drums we had.
 for (let i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-  function handleClick() {
-    alert("This button has been clicked");
-  }
+    this.style.color = 'white';
+
+
+
+  });
 
 
 }
+
+
+// function handleClick() {
+//   let audio = new Audio("sounds/tom-1.mp3");
+//   audio.play();
